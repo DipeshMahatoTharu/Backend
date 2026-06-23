e8f40cacfbd446fca0292af87ed87443686da451
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-06-23 10:05:36 +0545

    day 4 completed

--- python basic/Day4/function.py
+++ python basic/Day4/function.py
@@ -29,3 +29,88 @@
 
 # show_name()
 
+# Day 4 - Question 3 (Next Level)
+
+# Create a function:
+
+# def add():
+
+# Inside it:
+
+# Create two variables:
+
+# a = 10
+# b = 20
+# Print their sum.
+
+# Expected output:
+
+# 30
+
+# Call the function once.
+# def add():
+#     a = 10
+#     b = 20
+#     print(a + b)
+
+# add()
+# Next Level (Question 4)
+
+# Now let’s upgrade slightly:
+
+# Create a function:
+# def subtract(a, b):
+
+# Inside:
+
+# subtract a and b
+# print result
+
+# Then:
+
+# take input from user
+# call function
+# Expected flow:
+# Enter num one: 50  
+# Enter num two: 20  
+# 30
+
+
+
+# def subtract(a,b):
+#     print(a-b)
+    
+# a=int(input("Enter num one :"))
+# b=int(input("Enter num two :"))
+
+# subtract(a,b)
+
+    
+    
+# Next Question (Level up)
+# Create a function:
+# def check_number(n):
+
+# Inside:
+
+# if number is positive → print "Positive"
+# if number is negative → print "Negative"
+# if zero → print "Zero"
+
+# Then:
+
+# take input from user
+# call function
+
+
+
+def check_number(n):
+    if n > 0:
+        print("Number is Positive :")
+    elif n < 0 :
+        print("Number is Negative")
+    else:
+        print("Zero")
+    
+b=int(input("Enter num  :"))   
+check_number(b)
