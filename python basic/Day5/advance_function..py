a3784eb065ed5c67ab8f34185a43f5641946ff4e
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-06-23 10:14:35 +0545

    day 5 completed

---
+++ python basic/Day5/advance_function..py
@@ -0,0 +1,84 @@
+# Day 5 Practice (Start now)
+# 🟢 Question 1
+
+# Create a function:
+
+# def multiply(a, b):
+
+# 👉 It should:
+
+# return a × b
+
+# Then:
+
+# call function with 5 and 3
+# print result
+
+
+# def multiply(a, b):
+#     return a*b
+
+# c=21
+# f=31
+
+# print(multiply(c,f))
+
+
+
+# Question 2: Square function
+
+# Create:
+
+# def square(n):
+
+# It should:
+
+# return n × n
+
+# Test with:
+
+# 4
+# 7
+
+#  Print results outside function
+
+
+
+
+# def square(n):
+#     return n*n
+
+# a=4
+# print((square(a)))
+# b=7
+# print((square(b)))
+
+
+
+# 🔥 Day 5 status
+
+# You completed:
+
+# ✔ multiply function
+# ✔ square function
+
+# Next level is last challenge 👇
+
+# 🧪 FINAL Day 5 Challenge
+# Create:
+# def is_even(n):
+# Rules:
+# return "Even" if number is divisible by 2
+# return "Odd" otherwise
+# Test with:
+# print(is_even(10))
+# print(is_even(7))
+
+
+def is_even(n):
+    if n %2 ==0:
+        return "Even"
+    else:
+        return "Odd"
+    
+print(is_even(10))
