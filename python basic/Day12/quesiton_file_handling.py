5b4fe94cbe12ce1168d9fbe1af238b725e1215f4
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-06-27 19:58:41 +0545

    updated day 12 file handing

--- python basic/Day12/quesiton_file_handling.py
+++ python basic/Day12/quesiton_file_handling.py
@@ -1,6 +1,9 @@
 # 🟢 Q1 — Write to file
 
 # Create a file called notes.txt and write:
+# file=open("D:/Backend/python basic/Day12/notes.txt","w")
+# file.write("Python is easy \nBackend is powerful \nI am learning Django \n")
+
 
 # Python is easy
 # Backend is powerful
@@ -9,39 +12,63 @@
 # 👉 Then read and print it.
 
 # 🟢 Q2 — Append data
+# file=open("D:/Backend/python basic/Day12/notes.txt","a")
+# file.write("File handling is important\n")
 
+# file.close()
 # Add this line to the same file:
 
 # File handling is important
 
-# 👉 Then read and print again.
+#  Then read and print again.
+file=open("D:/Backend/python basic/Day12/notes.txt","r")
 
-# 🟢 Q3 — Count words
+content=file.read()
+# #  Q3 — Count words
 
-# Read the file and print:
-# 👉 Total number of words
+# print(content)
+# words=content.split()
+# # (Hint: use split())
+# print(words)
 
-# (Hint: use split())
-
-# 🟢 Q4 — Find word
+#  Q4 — Find word
 
 # Check if "Django" exists in file:
-
+if "Django" in content:
+    print("found")
+else:
+    print("Not found")
 # Print:
 
 # "Found"
 # "Not Found"
-# 🟢 Q5 — Line count
 
+
+#  Q5 — Line count
+countingline=0
 # 👉 Print how many lines are in the file
+spliting_lines=content.splitlines()
+for count in spliting_lines:
+    countingline +=1
+    
+print(countingline)
 
-# (Hint: splitlines())
+newtext=content.replace("Python","Java")
 
-# 🔥 BONUS CHALLENGE
-# Q6 — Replace word
+print(newtext)
 
 # Replace:
 
 # Python → Java
 
-# 👉 Save updated content in file
+#  Save updated content in file
+
+updatefile=open("D:/Backend/python basic/Day12/notes.txt","w")
+updatefile.write(newtext)
+updatefile.close() 
+
+updatefile=open("D:/Backend/python basic/Day12/notes.txt","r")
+print(updatefile.read())
+updatefile.close() 
+
+
