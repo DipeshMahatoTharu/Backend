bab2a30fb79e6e3391daba24ba0f81464685912c
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-07-01 15:04:37 +0545

    first inheritance

--- python basic/Day14/Inheritance.py
+++ python basic/Day14/Inheritance.py
@@ -6,7 +6,7 @@
 
 # Instead of writing the same code again and again, we inherit it.
 
-# Real-life example 👨‍👦
+# Real-life example 
 
 # Imagine:
 
@@ -30,3 +30,46 @@
 # Bird inherits them.
 
 
+
+
+# our First Exercise
+
+# Without looking at my code, write this yourself.
+
+# Parent Class
+# Vehicle
+
+class Vehicle:
+    def drive(self):
+        print("Vehicle Started")
+
+class Car(Vehicle):
+    pass
+
+car1=Car()
+car1.drive()
+
+    
+# Method:
+
+# start()
+
+# Output:
+
+# Vehicle Started
+# Child Class
+# Car
+
+# Car should inherit from Vehicle.
+
+# Create:
+
+# car1 = Car()
+
+# Call:
+
+# car1.start()
+
+# Expected Output:
+
+# Vehicle Started
