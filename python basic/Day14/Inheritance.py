52db54ac98d413afb4b6854e566d2194330cdd2b
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-07-01 14:59:07 +0545

    update started inheritance

---
+++ python basic/Day14/Inheritance.py
@@ -0,0 +1,32 @@
+# What is Inheritance?
+
+# Inheritance means:
+
+# A child class can use the code of a parent class.
+
+# Instead of writing the same code again and again, we inherit it.
+
+# Real-life example 👨‍👦
+
+# Imagine:
+
+# Animal
+# │
+# ├── Dog
+# ├── Cat
+# └── Bird
+
+# Every animal can:
+
+# Eat 
+# Sleep 
+
+# So instead of writing eat() and sleep() inside every class, we write them once in Animal.
+
+# Then:
+
+# Dog inherits them.
+# Cat inherits them.
+# Bird inherits them.
+
+
