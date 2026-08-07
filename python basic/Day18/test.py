7c705348f8f95bfaea2c09804be1a81d41da523e
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-08-07 13:00:06 +0545

    need some debugging in inventry

--- python basic/Day18/test.py
+++ python basic/Day18/test.py
@@ -573,11 +573,24 @@ class Inventory:
     def __init__(self,productname ,quantity=0):
         self.productname=productname
         self.quantity=quantity
-    def add_stock(self):
-         
+    def add_stock(self,stocks):
+         self.quantity +=stocks
+    def sell_stock(self,stocks):
+        if self.stocks < self.quantity:
+            print("Yout stock is 0")
+        else:
+            self.quantity -=stocks
         
+    def show_stock(self):
+        print("Current :",self.quantity)
+     
+        
+inventory1=Inventory("Shoes")
 
+inventory1.add_stock(3)
+inventory1.show_stock()
 
+inventory1.show_stock()
 
 #  Challenge (No Help)
 
