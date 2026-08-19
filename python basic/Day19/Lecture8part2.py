8289eb9fc81d240b5887277940dc4558dc55a5ae
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-08-19 20:20:16 +0545

    completed test

--- python basic/Day19/Lecture8part2.py
+++ python basic/Day19/Lecture8part2.py
@@ -1,1 +1,42 @@
-youtubelink=https://www.youtube.com/watch?v=bAwmZVJeO5s&t=1985s
+# youtubelink=https://www.youtube.com/watch?v=bAwmZVJeO5s&t=1985s
+
+# class Student:
+#     def __init__(self,name):
+#         self.name=name
+
+# s1=Student("Dipesh")
+
+# del s1.name
+# print(s1.name)
+
+
+# class Account:
+#     def __init__(self,Acc_name,Acc_pass):
+#         self.Acc_name=Acc_name
+#         self.__Acc_pass=Acc_pass
+
+#     def reset(self):
+#         print(self.__Acc_pass)
+
+# acc1=Account("3131414114414","sakdmasd")
+
+# acc1.Acc_name
+# # acc1.reset()
+# acc1.__Acc_pass
+
+
+
+class Car:
+    @staticmethod
+    def start():
+        print("Car Started")
+    @staticmethod   
+    def stop():
+        print("car Stoped ..")
+class Toyota(Car):
+    def __init__(self,name):
+        self.name=name
+
+car1=Toyota("Fortunar")
+car1.start()
+car1.stop()
---
+++ python basic/Day19/abstraction_encapluation.py
@@ -0,0 +1,11 @@
+# 1. Abstraction
+
+# Create an abstract class Payment.
+
+# It must have an abstract method:
+
+# pay()
+# Use ABC and @abstractmethod.
+
+class Payment:
+    
