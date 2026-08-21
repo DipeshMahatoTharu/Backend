c11843b65f2a94acf510e94264a26a466bc98f3d
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-08-21 20:26:33 +0545

    completed test

--- python basic/Day19/Lecture8part2.py
+++ python basic/Day19/Lecture8part2.py
@@ -37,6 +37,25 @@ class Toyota(Car):
     def __init__(self,name):
         self.name=name
 
-car1=Toyota("Fortunar")
+class Fortuner(Toyota):
+    def __init__(self, type):
+        self.type=type
+        
+    
+car1=Fortuner("diseal")
 car1.start()
 car1.stop()
+
+
+#multiclass inheritance
+class A:
+    var1="var for class a "
+class B:
+    var2="var for class b"
+class C(A,B):
+    var3="var for class c"
+
+test=C()
+print(test.var1)
+print(test.var2)
+print(test.var3)
