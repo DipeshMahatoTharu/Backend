dc2f36f9d655a67b1ce1bf672fdeec9f01c8b15a
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-08-30 21:56:15 +0545

    lecture

--- python basic/Day20/testoop.py
+++ python basic/Day20/testoop.py
@@ -92,22 +92,22 @@ stu3=Student("ssad",54,278059,85)
 
 # Which things belong to each object?
 
-# class Food:
-#     def __init__(self,name,address,phone_number):
-#         self.name=name
-#         self.address=address
-#         self.phone_number=phone_number
-#     def order(self,foodname,quantity,price):
-#        self.foodname=foodname
-#        self.quantity=quantity
-#        self.price=price
+class Food:
+    def __init__(self,name,address,phone_number):
+        self.name=name
+        self.address=address
+        self.phone_number=phone_number
+    def order(self,foodname,quantity,price):
+       self.foodname=foodname
+       self.quantity=quantity
+       self.price=price
        
-#     def total_price(self):
-#         print(self.quantity * self.price) 
+    def total_price(self):
+        print(self.quantity * self.price) 
         
-# order1=Food("Dipesh","Sifal",9860793587)
-# order1.order("Achha wala Pizza " ,2,120)
-# order1.total_price()
+order1=Food("Dipesh","Sifal",9860793587)
+order1.order("Achha wala Pizza " ,2,120)
+order1.total_price()
 
 
 # 🟢 Q3 — Bank Account
