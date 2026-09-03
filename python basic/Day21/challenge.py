085098b504f2c177e82006dfd68f01387022efb6
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-09-03 09:23:44 +0545

    day 21 challenge task 1

--- python basic/Day21/challenge.py
+++ python basic/Day21/challenge.py
@@ -9,14 +9,7 @@ in lists versus dictionaries.
 
 
 
-------------------------------------------------------------
-STEP 2: DICTIONARY-BASED STUDENT MANAGER
-------------------------------------------------------------
-Refactor your entire code to use a dictionary where the keys are student IDs:
-`self.students = {"S001": {"name": "Name", "marks": 85}}`
 
-Ensure all methods (add, find, update, delete, average, top) work identical 
-to the list version.
 
 ------------------------------------------------------------
 STEP 3: ANALYZE THE DIFFERENCE
@@ -54,11 +47,14 @@ ____________________________________________________
 
 # Requirements:
 # - `add_student(student_id, name, marks)`: Adds the record. If ID already exists, do not add.
-# - `find_student(student_id)`: Searches the list. Returns the student dictionary or None.
+# - `find_student(student_id)`: 
 # - `update_marks(student_id, new_marks)`: Searches, updates, returns True. Returns False if not found.
 # - `delete_student(student_id)`: Removes the student from the list.
 # - `get_average_marks()`: Calculates the average marks of all students.
 # - `get_top_student()`: Returns the student record with the highest marks.
+
+
+
 
 class Student:
     def __init__(self):
@@ -66,16 +62,70 @@ class Student:
 
     def add_student(self,student_id ,name ,marks):
         for record in self.students:
-            if record[id] ==student_id:
+            if record["id"] ==student_id:
                 print("student already exit")
                 return
+        
         new_student={"id":student_id ,"name":name ,"marks":marks}
+        
         self.students.append(new_student)
-
-
-
-# stduent1=Student()
-# print(stduent1.add_student("31131331","dipesh",90))
-    
+        print(f"{name} is added")
+# Searches the list. Returns the student dictionary or None.
+    def find_student(self,student_id):
+        for find in self.students:
+            if find["id"] == student_id:
+                return find
+            return None
+# update_marks(student_id, new_marks)`: Searches, updates, returns True. Returns False if not found.           
+    def update_marks(self,student_id,new_marks):
+        for update in self.students:
+            if update["id"]==student_id:
+               update["marks"]= new_marks
+               return True
+        return False
+# - `delete_student(student_id)`: Removes the student from the list.
+    def delete_student(self,student_id):
+        for delete in self.students:
+            if delete["id"]==student_id:
+                self.students.remove(delete)
+                return True
+        return None
+# - `get_average_marks()`: Calculates the average marks of all students.
+    def get_average_marks(self):
+        total_marks=0
+        if len(self.students)==0:
+            return 0
+        for marks in self.students:
+           total_marks += marks["marks"]
+           
+        return total_marks/len(self.students)
+# - `get_top_student()`: Returns the student record with the highest marks.       
+    def get_top_student(self):
+        hightestmark=0
+        top_student=None
+        for student in self.students:
+            if student["marks"] > hightestmark:
+                 hightestmark = student["marks"]
+                 top_student=student
+        return top_student 
+                
+            
+           
             
         
+                
+stduent1=Student()
+stduent1.add_student("31131331","dipesh",90)
+print(stduent1.find_student("12"))
+print(stduent1.update_marks("31131331",50))
+# print(stduent1.delete_student("31131331"))
+print(stduent1.get_average_marks())
+print(stduent1.get_top_student())  
+            
+# ------------------------------------------------------------
+# STEP 2: DICTIONARY-BASED STUDENT MANAGER
+# ------------------------------------------------------------
+# Refactor your entire code to use a dictionary where the keys are student IDs:
+# `self.students = {"S001": {"name": "Name", "marks": 85}}`
+
+# Ensure all methods (add, find, update, delete, average, top) work identical
