f9ec1a0b9eb4152272100b958736abae98d32032
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-09-04 08:34:17 +0545

    day 22 debugging  completed

--- python basic/Day22/challenge.py
+++ python basic/Day22/challenge.py
@@ -66,3 +66,56 @@ ____________________________________________________
 ____________________________________________________
 ____________________________________________________
 """
+
+# Implement the following independent functions:
+
+# 1. `validate_order(order_dict)`: 
+#    - Accepts a dictionary (e.g., `{"items": [{"name": "A", "price": 10, "qty": 2}], "coupon": "SAVE10"}`).
+#    - Returns True if order is valid (has items, items have positive price/qty). Returns False otherwise.
+def validate_order(order_dict):
+   item_list=order_dict["items"]
+   if len(item_list) == 0:
+      return False 
+   for item in item_list:
+      if item["price"] <= 0 or  item["qty"] <= 0:
+            return False
+   return True
+      
+# 2. `calculate_total(items)`:
+#    - Sums the subtotal price (price * qty) of all items in the list.
+def calculate_total(items):
+   total=0
+   if len(items) == 0:
+      return False
+   for item in items:
+      total=total + (items["price"] * items["qty"])
+
+   return total
+
+
+# 3. `apply_discount(subtotal, coupon_code)`:
+#    - If coupon is "SAVE10", subtract 10% from the subtotal.
+#    - If coupon is "SAVE20", subtract 20%.
+#    - Otherwise, return original subtotal.
+def apply_discount(subtotal,coupon_code):
+   
+   if coupon_code == "SAVE10 ":
+      subtotal = subtotal / (10/100)
+      return subtotal
+   elif coupon_code == "SAVE20":
+      subtotal =subtotal/ (20/100)
+      return subtotal
+   else:
+      return subtotal
+   
+   
+# 4. `calculate_tax(amount, tax_rate=0.13)`:
+#    - Multiplies amount by tax_rate (default 13% VAT).
+def calculate_tax(amount ,tax_rate=0.13):
+   return amount * tax_rate
+   
+# 5. `format_order_summary(order_dict, final_total)`:
+#    - Returns a string summarizing order details.
+def format_order_summary(order_dict,final_total):
+   return f"Orders are :{order_dict} and total is {final_total}"
+   
