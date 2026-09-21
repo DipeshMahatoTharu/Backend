77b79cb535ecc9c5118771e8840e9a5235bf6950
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-09-21 22:54:49 +0545

    practice finish

--- python basic/Day27_SQL_Fundamentals/debugging.py
+++ python basic/Day27_SQL_Fundamentals/debugging.py
@@ -19,8 +19,7 @@ def buggy_create_tables(conn: sqlite3.Connection):
             title TEXT NOT NULL,
             author_id INTEGER REFERENCES authors(id)
         );
-    """)
-    
+    """))
     cursor.execute("""
         CREATE TABLE authors (
             id INTEGER PRIMARY KEY,
